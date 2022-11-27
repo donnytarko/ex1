6344e3c110b0589c1bf5f8d61723282e856f220c
Dani <dani@commenda.io>
2022-11-27 18:59:25 +0200

    alkdj

--- tool/main.c
+++ tool/main.c
@@ -11,8 +11,8 @@
 int main(int argc, char** argv) {
     FILE* inputFile = fopen(argv[INPUT_FILE], "r");
     FILE* outputFile = fopen(argv[OUTPUT_FILE], "w");
-    
-    if (!strcmp(argv[FLAG],"-e")) {
+
+    if (argv[FLAG][1] == 'e') {
         RLEList list = asciiArtRead(inputFile);
         asciiArtPrintEncoded(list, outputFile);
         RLEListDestroy(list);        
